c8f5f43cc264b8b65cc8e13824c1b5fd8a526a54
javierc22 <javier.ec.22@gmail.com>
2020-07-26 23:21:04 -0400

    Insertar usuarios en la BD

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,16 @@
-exports.createUser = (req, res) => {
-  console.log(req.body);
+const User = require('../models/User')
+
+exports.createUser = async (req, res) => {
+  try {
+    let user;
+    // Crea el nuevo usuario
+    user = new User(req.body);
+    // Guardar usuario
+    await user.save();
+    // Mensaje de confirmación
+    res.send('Usuario creado correctamente');
+  } catch (error) {
+    console.log(error);
+    res.status(400).send('Hubo un error');
+  }
 }
