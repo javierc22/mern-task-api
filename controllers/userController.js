af19be9e882ab6f8501d2af694bfe7f693fcc8e9
javierc22 <javier.ec.22@gmail.com>
2020-07-26 23:43:57 -0400

    Revisar si el usuario a crear existe o no

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,14 +1,23 @@
 const User = require('../models/User')
 
 exports.createUser = async (req, res) => {
+  // Extraer email y password
+  const { email, password } = req.body;
+
   try {
-    let user;
+    // Revisar que el usuario registrado sea único
+    let user = await User.findOne({ email });
+
+    if (user) {
+      return res.status(400).json({ msg: 'El usuario ya existe'});
+    }
+
     // Crea el nuevo usuario
     user = new User(req.body);
     // Guardar usuario
     await user.save();
     // Mensaje de confirmación
-    res.send('Usuario creado correctamente');
+    res.json({ msg: 'Usuario creado correctamente'});
   } catch (error) {
     console.log(error);
     res.status(400).send('Hubo un error');
