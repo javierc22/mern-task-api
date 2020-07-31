05aea452328c8cf000a9e450c09cdf401bae1a9e
javierc22 <javier.ec.22@gmail.com>
2020-07-31 19:32:47 -0400

    Insertando nuevos Proyectos

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -1,4 +1,14 @@
+const Project = require('../models/Project');
 
 exports.createProject = async (req, res) => {
+  try {
+    // Crear nuevo proyecto
+    const project = new Project(req.body);
+    project.save()
+    res.json(project);
 
+  } catch (error) {
+    console.log(error);
+    res.status(500).send('Hubo un error');
+  }
 }
